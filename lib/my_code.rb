475cdd9381a90663a494f1c9360673ff6a7eb561
Valentine Maillard <valentinemaillard1@gmail.com>
2019-10-30 21:59:47 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
 # Your Code Here
+#def map(element1,&block); element1.map(&block); end
+
+def map(array)
+  new = []
+  i = 0
+  while i < array.length
+    new.push(yield(array[i]))
+    i += 1
+  end
+  new
+end
+
+def reduce(source_array, starting_point=nil)
+  if starting_point
+    accum = starting_point
+    i = 0
+  else
+    accum = source_array[0]
+    i = 1
+  end
+  while i < source_array.length
+    accum = yield(accum, source_array[i])
+    i += 1
+  end
+  accum
+end
